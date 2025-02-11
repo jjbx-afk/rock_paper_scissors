a3fea2710ec0d1ef9531984f19d016776155cded
Bayane Chakir <bayane.pbg@gmail.com>
2025-02-11 12:24:45 +0100

    rewriting old exercice and testing functions

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
-//variables du score réutiliser dans les fonctions 
-let humanScore; 
-let computerScore;
+
+
+// variables du score réutiliser dans les fonctions 
+
 
 
 // random computer choice
 function getComputerChoice() {  
 
     let random = Math.floor(Math.random() * 3);
-    let choice;
 
     if (random === 0) {
         choice = "rock";
@@ -23,76 +23,52 @@ function getComputerChoice() {
     }
 
     console.log("Computer's choice", choice);
+
     return choice;
 
 } 
 
-// game du jeu 
-function playGame() {
-    humanScore = 0;
-    computerScore = 0;
-    //boucle pour répeter la procédur pour chaque round
-    for (let i = 0; i <= 4; i++) { 
-        // console.log("%cRound " + Number(i+1), "font-weight:bold")
-        playRound();
-        console.log("Human score: ", humanScore);
-        console.log("Computer score: ", computerScore);
-    }
-    //affichage du score finale 
-    console.log("Final scores => Human: " + humanScore + ", Computer: " + computerScore + "."); 
-    //affichage de message de victoire/échec a l'aide d'une condition  
-    console.log(humanScore > computerScore ? "You win" : (humanScore < computerScore ? "you loose" : "tie")); 
-    
-    // façon + verbale pour reproduire la condition d'en haut : 
 
-    // if (humanScore > computerScore) {
-    //     console.log("You win");
-    // } else {
-    //     if (humanScore < computerScore) {
-    //         console.log("you loose");
-    //     } else {
-    //         console.log("tie")
-    //     }
-    // }
-}
-// input for human choice
 function getHumanChoice() {  
 
-     let choice = prompt("rock, paper or scissors? :").toLowerCase()
- 
+        let choice = ("rock, paper or scissors? :").toLowerCase();
+        let inputChoice = prompt(choice);
+    
     if (["rock", "scissors", "paper"].includes(choice)) {
         console.log("your choice: ", choice);
-        return choice;
+
     } else { 
-        console.error("Choix invalide, les choix possibles sont: paper, scissors, rock")
+        console.log(inputChoice);
+        console.error("Choix invalide, les choix possibles sont: paper, scissors, rock");
     }
 
-
-    // Si "choice" n'est pas une des trois valeurs possibles
-    // Alors faire
-    // return choice;
 }
 
-//fonction permettant de jouer un round et repris dand la boucle for dans la fonction de la game
-function playRound() {
+let humanScore = 0; 
+let computerScore = 0;
 
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
+function playRound(humanChoice, computerChoice) {
+
 
     if (humanChoice == "rock" && computerChoice == "scissors"){
         humanScore++;
         return "You Win! Rock beats scissors";
+
     } else if (humanChoice == "rock" && computerChoice == "paper"){
         computerScore++;
+
         return "You lose.. Paper beats rock"; 
+
     } else if (humanChoice == "paper" && computerChoice == "rock"){
-        // humanScore = humanScore + 1;
-        // humanScore += 1;
         humanScore++;
+
         return "You Win! Paper beats rock ";
+
     } else if (humanChoice == "paper" && computerChoice == "scissors"){
         computerScore++;
+
         return "You lose.. Scissors beats paper"; 
+
     } else if (humanChoice == "scissors" && computerChoice == "paper"){
         humanScore++;
         return "You Win! Scissiors beats paper"; 
@@ -106,24 +82,49 @@ function playRound() {
     }                                                          
 } 
 
-// console.log(playRound(humanSelection, computerSelection))
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
+
+console.log(playRound(humanSelection, computerSelection))
+
+console.log(playGame);
+
+
+// playGame() //relancer une nouvelle game 
+
+// // playGame()
+
+// restartingChoice = prompt("do you want to restart ? (yes/no)").toLowerCase();
+
+// if (restartingChoice === "yes"){
+//      playGame()
+// }
+// else if (restatignChoice === "no") {
+
+//   die("Thanks for playing! see ya!");
+
+// }
 
 
 
-// console.log(playGame);
+// game du jeu 
+// function playGame() {
+//     humanScore = 0;
+//     computerScore = 0;
+//     //boucle pour répeter la procédur pour chaque round
+//     for (let i = 0; i <= 4; i++) { 
+//         // console.log("%cRound " + Number(i+1), "font-weight:bold")
+//         playRound();
+//         console.log("Human score: ", humanScore);
+//         console.log("Computer score: ", computerScore);
+//     }
+//     //affichage du score finale 
+//     console.log("Final scores => Human: " + humanScore + ", Computer: " + computerScore + "."); 
+//     //affichage de message de victoire/échec a l'aide d'une condition  
+//     console.log(humanScore > computerScore ? "You win" : (humanScore < computerScore ? "you loose" : "tie")); 
+    
+//     // façon + verbale pour reproduire la condition d'en haut : 
 
-
-playGame() //relancer une nouvelle game 
-
-// playGame()
-
-restartingChoice = prompt("do you want to restart ? (yes/no)").toLowerCase();
-
-if (restartingChoice === "yes"){
-     playGame()
-}
-else if (restatignChoice === "no") {
-
-  die("Thanks for playing! see ya!");
-
-}
+// }
